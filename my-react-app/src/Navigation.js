6e6993b114cbd738499a6b771df0e0f6e6ce468f
tkomm4 <kommuri.tarun@gmail.com>
2023-05-01 16:48:52 -0400

    commiting the website and homepage

---
+++ my-react-app/src/Navigation.js
@@ -0,0 +1,14 @@
+export default function Navigation(){
+    return (<nav className="nav">
+        <a href="/" className="site-title">Hack4Impact 2023 Final Project</a>
+        <ul>
+            <li>
+                <a href="/movies">Our Pages</a>
+            </li>
+            <li>
+                <a href="/anime">Team Roles</a>
+            </li>
+        </ul>
+    </nav>
+    )
+}
