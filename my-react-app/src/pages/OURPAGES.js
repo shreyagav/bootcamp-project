182f3e1f07c5de43fc83fc7391f3361c5d60a1ad
tkomm4 <kommuri.tarun@gmail.com>
2023-05-02 06:35:11 -0400

    added the ourpages page

--- my-react-app/src/pages/OURPAGES.js
+++ my-react-app/src/pages/OURPAGES.js
@@ -1,5 +1,48 @@
 export default function OURPAGES(){
     return(
-        <h1>OURPAGES</h1>
+        <div className="container">
+      <header>
+        <div className="logo-container">
+          <img
+            src="https://media.licdn.com/dms/image/C560BAQGDbdhpmxRmrg/company-logo_200_200/0/1633548684667?e=2147483647&v=beta&t=uzgi1imN65DlpVvbd0TUb-S1L4pTNXwoS0tkolTipKs"
+            alt="Hack4Impact logo"
+          />
+          <h1>OUR PAGES</h1>
+          <img
+            src="https://media.licdn.com/dms/image/C560BAQGDbdhpmxRmrg/company-logo_200_200/0/1633548684667?e=2147483647&v=beta&t=uzgi1imN65DlpVvbd0TUb-S1L4pTNXwoS0tkolTipKs"
+            alt="Hack4Impact logo"
+          />
+        </div>
+        <div className="navigate">
+          <p>Navigate through the pages of each of our member's about me pages.</p>
+        </div>
+      </header>
+      <main>
+        <div className="members">
+          <div className="row">
+            <a className="button" href="member1-react-app-url">
+              Abhinav Mukherjee
+            </a>
+            <a className="button" href="member2-react-app-url">
+              Shreya Gavarapattu
+            </a>
+            <a className="button" href="member3-react-app-url">
+              Shreyes Verde
+            </a>
+          </div>
+          <div className="row">
+            <a className="button" href="member4-react-app-url">
+              Aditi Chalasani
+            </a>
+            <a className="button" href="member5-react-app-url">
+              Tarun Kommuri
+            </a>
+            <a className="button" href="member6-react-app-url">
+              Will Phoone
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
     ) 
 }
