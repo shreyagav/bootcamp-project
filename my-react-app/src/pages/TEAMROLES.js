89b79249b95a99abed2283ba19a0febeb3aa619e
tkomm4 <kommuri.tarun@gmail.com>
2023-05-02 06:29:26 -0400

    added the team roles section

--- my-react-app/src/pages/TEAMROLES.js
+++ my-react-app/src/pages/TEAMROLES.js
@@ -1,7 +1,66 @@
 export default function TEAMROLES(){
     return(
         <div>
+            <html>
+    <head>
+        <title>Team Roles</title>
+        <link rel="stylesheet" href="teamRoles.css"/>
+    </head>
 
+    <body>
+        <div id="headerContainer">
+            <h1 id="headerText">Team Roles</h1>
+        </div>
+        <div class="row">
+            <div>
+                <div>
+                    <p class="titleName">Project Manager</p>
+                    <div>
+                        <p class="actualName">WILLIAM PHOONE</p>
+                        <p class="aspiringText">aspiring role: </p>
+                    </div>
+                </div>
+                <div>
+                    <p class="titleName">Designer</p>
+                    <div>
+                        <p class="actualName">ADITI CHALASANI</p>
+                        <p class="aspiringText">aspiring role: project manager</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="titleName">Tech Lead</p>
+                    <div>
+                        <p class="actualName">SHREYA GAVARAPATTU</p>
+                        <p class="aspiringText">aspiring role: </p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p class="titleName">Engineer</p>
+                    <div>
+                        <p class="actualName">SHREYES VARDE</p>
+                        <p class="aspiringText">aspiring role: </p>
+                    </div>
+                </div>
+                <div>
+                    <p class="titleName">Engineer</p>
+                    <div>
+                        <p class="actualName">ABHINAV MUKHERJEE</p>
+                        <p class="aspiringText">aspiring role: </p>
+                    </div>
+                </div>
+                <div>
+                    <p class="titleName">Engineer</p>
+                    <div>
+                        <p class="actualName">TARUN KOMMURI</p>
+                        <p class="aspiringText">aspiring role: </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
         </div>
     ) 
 }
